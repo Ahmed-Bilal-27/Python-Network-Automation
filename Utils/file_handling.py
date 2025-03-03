49f3fb47c990b8ad8f439cbed414a9fb9829a51b
Ahmed-Bilal_27 <ahmedbilal3830@gmail.com>
2025-03-03 12:00:26 +0500

    Updated write method to include the command in the filename whose output is being written.

--- Utils/file_handling.py
+++ Utils/file_handling.py
@@ -1,9 +1,9 @@
 import time
 
-def write_data(script, data, file_type = '.txt'):
+def write_data(script, data, cmd = None, file_type = '.txt'):
     timestamp = f'{time.localtime().tm_mday}-{time.localtime().tm_mon}-{time.localtime().tm_year}_{time.localtime().tm_hour}-{time.localtime().tm_min}-{time.localtime().tm_sec}'
     script_name = script.split('/')[-1].split('.')[0]
-    filename = f'{timestamp} {script_name}'
+    filename = f'{timestamp} {cmd} {script_name}'
     path = f'../Output_Files/{filename}{file_type}'
     with open(path, 'w') as file:
         file.write(data)
